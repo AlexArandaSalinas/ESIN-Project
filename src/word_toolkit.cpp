65a7f1ac9480abf82e743d1f0322bf10ae878ba8
Toboso-panda <daniel.toboso@estudiantat.upc.edu>
2024-11-25 16:50:51 +0100

    Update word_toolkit.cpp

--- src/word_toolkit.cpp
+++ src/word_toolkit.cpp
@@ -1,1 +1,43 @@
 #include "word_toolkit.hpp"
+#define _WORD_TOOLKIT_HPP
+#include <string>
+#include "stdio.h"
+#include <list>
+#include <esin/error>
+using namespace std;
+
+	
+bool word_toolkit::es_canonic(const string& s) throw(){
+	for (i=0;i<s.len()-1;i++){
+	    if (s[i+1] < s[i]) return false;
+		
+		}
+	 return true;
+	}
+
+string word_toolkit::anagrama_canonic(const string& s) throw(){
+	string s_canon = sort(s.begin(),s.end());
+	return s_canon;
+	}
+
+char word_toolkit::mes_frequent(const string& excl, const list<string>& L) throw(){
+	l = anagrama_canonic(L){
+		int contador = 0; //guarda el contador del caracter actual
+		int max=0;        //guarda el numero de veces que se repite el caracter más frecuente
+		char actual = l[0]; // guarda el caracter actual
+		char freq = l[0];	// guarda el caracter mas frecuente
+		for (i=0; i < l.len()-1 ; i++){
+			if (l[i]== actual){
+			contador++;
+			if(contador > max){
+				max = contador;
+				freq = s.[i];
+				}	
+			}else{
+				contador = 1;	
+				actual = l[i];
+			}
+		
+		}
+	return freq;
+	}
