8a41a63f0b366d70ec084e7ba2bf5f70f7a883db
Alex Aranda <aranda711@gmail.com>
2024-12-16 16:48:22 +0100

    Update word_toolkit.cpp

--- src/word_toolkit.cpp
+++ src/word_toolkit.cpp
@@ -1,8 +1,10 @@
 #include "word_toolkit.hpp"
 
+#include <algorithm> // Necesario para sort
+
 bool word_toolkit::es_canonic(const string &s) throw()
 {
-    for (unsigned int i = 0; i < s.len() - 1; i++)
+    for (unsigned int i = 0; i < s.size() - 1; i++)
     {
         if (s[i + 1] < s[i])
             return false;
@@ -12,7 +14,8 @@ bool word_toolkit::es_canonic(const string &s) throw()
 
 string word_toolkit::anagrama_canonic(const string &s) throw()
 {
-    string s_canon = sort(s.begin(), s.end());
+    string s_canon = s;
+    sort(s_canon.begin(), s_canon.end()); // Ordena el string
     return s_canon;
 }
 
@@ -24,7 +27,7 @@ char word_toolkit::mes_frequent(const string &excl, const list<string> &L) throw
     int max = 0;        // guarda el numero de veces que se repite el caracter más frecuente
     char actual = l[0]; // guarda el caracter actual
     char freq = l[0];   // guarda el caracter mas frecuente
-    for (unsigned int i = 0; i < l.len() - 1; i++)
+    for (unsigned int i = 0; i < l.size() - 1; i++)
     {
         if (l[i] == actual)
         {
@@ -43,3 +46,61 @@ char word_toolkit::mes_frequent(const string &excl, const list<string> &L) throw
     }
     return freq;
 }
+
+/*
+char word_toolkit::mes_frequent(const string &excl, const list<string> &L) throw()
+{
+    // Array para marcar las letras excluidas ('A' a 'Z')
+    bool excl_set[26] = {false}; // Inicializado a falso
+
+    // Marcar los caracteres en el string excl
+    for (unsigned int i = 0; i < excl.size(); ++i)
+    {
+        excl_set[excl[i] - 'A'] = true;
+    }
+
+    // Verificar si todas las letras están excluidas
+    bool all_excluded = true;
+    for (int i = 0; i < 26; ++i)
+    {
+        if (!excl_set[i])
+        {
+            all_excluded = false;
+            break;
+        }
+    }
+    if (all_excluded)
+        return '\0';
+
+    // Array para contar la frecuencia de cada letra ('A' a 'Z')
+    int freq[26] = {0};
+
+    // Recorrer la lista de palabras y contar frecuencias
+    for (list<string>::const_iterator it = L.begin(); it != L.end(); ++it)
+    {
+        const string &word = *it;
+        for (unsigned int i = 0; i < word.size(); ++i)
+        {
+            int index = word[i] - 'A';
+            if (!excl_set[index]) // Si el carácter no está excluido
+            {
+                freq[index]++;
+            }
+        }
+    }
+
+    // Encontrar el carácter más frecuente
+    char freq_char = '\0';
+    int max_count = 0;
+    for (int i = 0; i < 26; ++i)
+    {
+        if (freq[i] > max_count || (freq[i] == max_count && freq_char > ('A' + i)))
+        {
+            max_count = freq[i];
+            freq_char = 'A' + i;
+        }
+    }
+
+    return freq_char;
+}
+*/
