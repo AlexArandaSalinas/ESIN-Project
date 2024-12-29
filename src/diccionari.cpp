93879ee4c20fdf1a66faeb4395e8f24a9cb3355f
Toboso-panda <daniel.toboso@estudiantat.upc.edu>
2024-12-29 16:24:32 +0100

    implementacion diccionari.cpp
    me da error de version de c++ asi que no lo puedo compilar bien

--- src/diccionari.cpp
+++ src/diccionari.cpp
@@ -1,136 +1,106 @@
 #include "diccionari.hpp"
 
-#define _DICCIONARI_HPP
-#include <string>
-#include <list>
-#include <vector>
-#include <esin/error>
-#include <esin/util>
+// Constructor per defecte
+diccionari::diccionari() throw(error) {
+  root = nullptr;
+}
 
-using namespace std;
-using util::nat;
-
-class diccionari {
-public:
-    diccionari() throw(error);
-    diccionari(const diccionari& D) throw(error);
-    diccionari& operator=(const diccionari& D) throw(error);
-    ~diccionari() throw();
-
-    void insereix(const string& p) throw(error);
-    string prefix(const string& p) const throw(error);
-    void satisfan_patro(const vector<string>& q, list<string>& L) const throw(error);
-    void llista_paraules(nat k, list<string>& L) const throw(error);
-    nat num_pal() const throw();
-
-private:
-    struct Node {
-        char lletra;
-        bool final_paraula;
-        Node* esquerra;
-        Node* mig;
-        Node* dreta;
-
-        Node(char c) : lletra(c), final_paraula(false), esquerra(nullptr), mig(nullptr), dreta(nullptr) {}
-    };
-
-    Node* arrel;
-    nat num_paraules;
-
-    void insereix(Node*& n, const string& p, nat i) throw(error);
-    string prefix(Node* n, const string& p, nat i) const throw(error);
-    void satisfan_patro(Node* n, const vector<string>& q, nat i, string actual, list<string>& L) const throw(error);
-    void llista_paraules(Node* n, nat k, string actual, list<string>& L) const throw(error);
-    void destrueix(Node* n) throw();
-};
-
-#endif
-
-#include "diccionari.hpp"
-
-diccionari::diccionari() throw(error) : arrel(nullptr), num_paraules(0) {}
-
+// Constructor per còpia
 diccionari::diccionari(const diccionari& D) throw(error) {
-    // Implementació del constructor per còpia
+  root = copy(D.root);
 }
 
+// Operador d'assignació
 diccionari& diccionari::operator=(const diccionari& D) throw(error) {
-    // Implementació de l'operador d'assignació
-    return *this;
+  if (this != &D) {
+    clear(root);
+    root = copy(D.root);
+  }
+  return *this;
 }
 
+// Destructor
 diccionari::~diccionari() throw() {
-    destrueix(arrel);
+  clear(root);
 }
 
+// Insereix una paraula al diccionari
 void diccionari::insereix(const string& p) throw(error) {
-    insereix(arrel, p, 0);
+  root = insert(root, p, 0);
 }
 
-void diccionari::insereix(Node*& n, const string& p, nat i) throw(error) {
-    if (i == p.size()) return;
-    if (!n) n = new Node(p[i]);
-    if (p[i] < n->lletra) insereix(n->esquerra, p, i);
-    else if (p[i] > n->lletra) insereix(n->dreta, p, i);
-    else {
-        if (i == p.size() - 1) n->final_paraula = true;
-        else insereix(n->mig, p, i + 1);
+Node* diccionari::insert(Node* node, const string& p, int index) {
+  char c = p[index];
+  if (!node) {
+    node = new Node(c);
+  }
+  if (c < node->character) {
+    node->left = insert(node->left, p, index);
+  } else if (c > node->character) {
+    node->right = insert(node->right, p, index);
+  } else {
+    if (index + 1 == p.size()) {
+      node->is_end_of_word = true;
+    } else {
+      node->middle = insert(node->middle, p, index + 1);
     }
+  }
+  return node;
 }
 
+// Retorna el prefix més llarg de p que és una paraula del diccionari
 string diccionari::prefix(const string& p) const throw(error) {
-    return prefix(arrel, p, 0);
+  return longest_prefix(root, p, 0, "");
 }
 
-string diccionari::prefix(Node* n, const string& p, nat i) const throw(error) {
-    if (!n || i == p.size()) return "";
-    if (p[i] < n->lletra) return prefix(n->esquerra, p, i);
-    else if (p[i] > n->lletra) return prefix(n->dreta, p, i);
-    else {
-        string res = p.substr(0, i + 1);
-        if (n->final_paraula) return res;
-        return prefix(n->mig, p, i + 1);
+string diccionari::longest_prefix(Node* node, const string& p, int index, string current_prefix) const {
+  if (!node) return current_prefix;
+  char c = p[index];
+  if (c < node->character) {
+    return longest_prefix(node->left, p, index, current_prefix);
+  } else if (c > node->character) {
+    return longest_prefix(node->right, p, index, current_prefix);
+  } else {
+    current_prefix += c;
+    if (node->is_end_of_word) {
+      return longest_prefix(node->middle, p, index + 1, current_prefix);
+    } else {
+      return longest_prefix(node->middle, p, index + 1, current_prefix);
     }
+  }
 }
 
+// Retorna les paraules que satisfan el patró especificat
 void diccionari::satisfan_patro(const vector<string>& q, list<string>& L) const throw(error) {
-    satisfan_patro(arrel, q, 0, "", L);
+  // Implementació específica aquí
 }
 
-void diccionari::satisfan_patro(Node* n, const vector<string>& q, nat i, string actual, list<string>& L) const throw(error) {
-    if (!n || i == q.size()) return;
-    if (q[i].find(n->lletra) != string::npos) {
-        actual += n->lletra;
-        if (i == q.size() - 1 && n->final_paraula) L.push_back(actual);
-        satisfan_patro(n->mig, q, i + 1, actual, L);
-        actual.pop_back();
-    }
-    satisfan_patro(n->esquerra, q, i, actual, L);
-    satisfan_patro(n->dreta, q, i, actual, L);
-}
-
+// Retorna una llista de paraules de longitud major o igual a k
 void diccionari::llista_paraules(nat k, list<string>& L) const throw(error) {
-    llista_paraules(arrel, k, "", L);
+  // Implementació específica aquí
 }
 
-void diccionari::llista_paraules(Node* n, nat k, string actual, list<string>& L) const throw(error) {
-    if (!n) return;
-    actual += n->lletra;
-    if (n->final_paraula && actual.size() >= k) L.push_back(actual);
-    llista_paraules(n->mig, k, actual, L);
-    actual.pop_back();
-    llista_paraules(n->esquerra, k, actual, L);
-    llista_paraules(n->dreta, k, actual, L);
-}
-
+// Retorna el nombre de paraules en el diccionari
 nat diccionari::num_pal() const throw() {
-    return num_paraules;
+  // Implementació específica aquí
+  return 0; // Retorna el nombre de paraules
 }
 
-void diccionari::destrueix(Node* n) throw() {
-    if (!n) return;
-    destrueix(n->esquerra);
-    destrueix(n->mig);
-    destrueix(n->dreta);
-    delete n;
+// Funcions auxiliars
+Node* diccionari::copy(Node* node) {
+  if (!node) return nullptr;
+  Node* new_node = new Node(node->character);
+  new_node->is_end_of_word = node->is_end_of_word;
+  new_node->left = copy(node->left);
+  new_node->middle = copy(node->middle);
+  new_node->right = copy(node->right);
+  return new_node;
 }
+
+void diccionari::clear(Node* node) {
+  if (node) {
+    clear(node->left);
+    clear(node->middle);
+    clear(node->right);
+    delete node;
+  }
