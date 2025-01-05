e67954c7973f39efec445efb5f385e9363f5a100
Alex Aranda <aranda711@gmail.com>
2025-01-05 12:31:10 +0100

    more test

--- src/driver_iter_subset.cpp
+++ src/driver_iter_subset.cpp
@@ -3,20 +3,53 @@
 
 using namespace std;
 
-int main() {
-    nat n = 4;
-    nat k = 2;
+void test_iter_subset(nat n, nat k) {
+    cout << "Testing iter_subset with n = " << n << " and k = " << k << endl;
     iter_subset it(n, k);
 
-
+    // Probar el operador de preincremento y el método is_finished
     while (!it._finished) {
-        vector<nat> subset = *it; 
+        vector<nat> subset = *it;
         for (size_t i = 0; i < subset.size(); ++i) {
             cout << subset[i] << " ";
         }
         cout << endl;
         ++it;
     }
+
+    // Probar el operador de postincremento
+    it = iter_subset(n, k); // Reiniciar el iterador
+    cout << "Testing post-increment operator" << endl;
+    while (!it._finished) {
+        vector<nat> subset = *it;
+        for (size_t i = 0; i < subset.size(); ++i) {
+            cout << subset[i] << " ";
+        }
+        cout << endl;
+        it++;
+    }
+
+    // Probar el operador de igualdad y desigualdad
+    iter_subset it2(n, k);
+    if (it == it2) {
+        cout << "Iterators are equal" << endl;
+    } else {
+        cout << "Iterators are not equal" << endl;
+    }
+
+    ++it2;
+    if (it != it2) {
+        cout << "Iterators are not equal after increment" << endl;
+    } else {
+        cout << "Iterators are still equal after increment" << endl;
+    }
+}
+
+int main() {
+    test_iter_subset(4, 2);
+    test_iter_subset(5, 3);
+    test_iter_subset(3, 3);
+    test_iter_subset(3, 4); // Caso donde k > n
 
     return 0;
 }
