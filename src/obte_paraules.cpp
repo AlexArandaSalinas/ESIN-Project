8eb03d0a1d2c02e1c735dfa26d54eda5fe692527
Toboso-panda <daniel.toboso@estudiantat.upc.edu>
2025-01-11 20:30:42 +0100

    Update obte_paraules.cpp coste

--- src/obte_paraules.cpp
+++ src/obte_paraules.cpp
@@ -2,14 +2,13 @@
 #include "iter_subset.hpp"
 #include "word_toolkit.hpp"
 
-// Ya que no hi ha un .rep, i no es poden modificar els .hpp, he posat aqui les funcions privades.
-bool comparar(const string &a, const string &b);
-void sortList(list<string> &paraules);
-
 /* Pre:  Cert
    Post: Retorna la llista de paraules que es poden formar usant k lletres
    de la paraula s. Llança error si k és major que la longitud de
-   l'string s o k < 3. */
+   l'string s o k < 3. 
+   Cost: O(C(n, k) * (k log k + m)), on n és la longitud de s,
+   C(n, k) és el nombre de combinacions possibles de k lletres de s
+   i m és el nombre de paraules retornades. */
 void obte_paraules::obte_paraules(nat k, const string &s, const anagrames &A, list<string> &paraules) throw(error)
 {
    if (k > s.length() || k < 3)
@@ -32,17 +31,15 @@ void obte_paraules::obte_paraules(nat k, const string &s, const anagrames &A, li
       paraules.insert(paraules.end(), temp.begin(), temp.end());
       ++it;
    }
-
-   sortList(paraules);
-
-   paraules.unique();
 }
 
 /* Pre:  Cert
    Post: Retorna la llista de paraules que es poden formar usant 3 o més lletres
    de la paraula s. La llista estarà ordenada de menys a més longitud;
    a igual longitud les paraules estan en ordre alfabètic creixent.
-   Llança un error si l'string s té menys de tres lletres. */
+   Llança un error si l'string s té menys de tres lletres.
+   Cost: O(2^n * (n log n + m)), on n és la longitud de s
+   i m és el nombre de paraules retornades. */
 void obte_paraules::obte_paraules(const string &s, const anagrames &A, list<string> &paraules) throw(error)
 {
    if (s.length() < 3)
@@ -69,31 +66,6 @@ void obte_paraules::obte_paraules(const string &s, const anagrames &A, list<stri
       }
    }
 
-   sortList(paraules);
-
+   paraules.sort();
    paraules.unique();
-}
-
-// Funciones privadas
-
-/*
-Pre: Cert
-Post: Devuelve true si la palabra 'a' es menor que la palabra 'b' tamaño.
-      Si tienen la misma longitud, se compara lexicográficamente.
-*/
-bool comparar(const string &a, const string &b)
-{
-   if (a.length() != b.length())
-   {
-      return a.length() < b.length(); // Primero por longitud
-   }
-   return a < b; // Si tienen la misma longitud, orden lexicográfico
-}
-
-/* Pre: Cert
-   Post: Ordena la lista de palabras 'l' de menor a mayor longitud y, en caso de empate,
-         de menor a mayor lexicográficamente */
-void sortList(list<string> &l)
-{
-   l.sort(comparar); // Usando la función de comparación
 }
